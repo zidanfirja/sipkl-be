9df618c149fe2ff2605c7022756089bd210c44dd
zidan <muhamadzidanfrjtllh@gmail.com>
2024-11-15 16:03:42 +0700

    feat: main.go

--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"fmt"
 	"go-gin-mysql/Database"
+	"go-gin-mysql/Models"
 	"go-gin-mysql/Routers"
 )
 
 func main() {
 	Database.ConnetDB()
-	// Menjalankan migrasi otomatis
-	// Database.AutoMigrate(&Models.DataSiswa{}, &Models.Role{}, &Models.Industri{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{})
 
-	// Database.AutoMigrate(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
+	Database.AutoMigrate(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
 	// Database.DB.Migrator().DropTable(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
 
 	// roleModel := Models.RoleModel(db)
 	// userService := Controller.RoleCOntroller(roleModel)
-
+	//
 	// Mengatur router Gin
 	router := Routers.SetupRouter()
 	fmt.Println("Run at localhost:8080")
