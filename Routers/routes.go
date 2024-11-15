f3d2d776d3ba594129a6f088b6de35c7f3e2d8d8
zidan <muhamadzidanfrjtllh@gmail.com>
2024-11-15 17:03:48 +0700

    feat: add route, vercel.json

--- Routers/routes.go
+++ Routers/routes.go
@@ -2,12 +2,20 @@ package Routers
 
 import (
 	Controllers "go-gin-mysql/Controller"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
+
 	route := gin.Default()
+
+	route.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"massage": "halaman pertama",
+		})
+	})
 
 	v1 := route.Group("/sipkl/v1/data/hubin/")
 	{
