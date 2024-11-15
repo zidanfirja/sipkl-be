838b7b403ac362f1d82b10fd553ee72c9198abb3
zidan <muhamadzidanfrjtllh@gmail.com>
2024-11-15 22:38:45 +0700

    feat: main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"go-gin-mysql/Database"
-	"go-gin-mysql/Models"
 	"go-gin-mysql/Routers"
 	"os"
 )
@@ -10,7 +9,7 @@ import (
 func main() {
 	Database.ConnetDB()
 
-	Database.AutoMigrate(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
+	// Database.AutoMigrate(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
 	// Database.DB.Migrator().DropTable(&Models.Role{}, &Models.Pegawai{}, &Models.KonfigurasiRoles{}, &Models.Industri{}, &Models.DataSiswa{})
 
 	// roleModel := Models.RoleModel(db)
